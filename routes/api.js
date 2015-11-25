d57820ef6083b740c67e4e75084d7fe0f9771212
LarryStanley <kill.xmurderer@gmail.com>
2015-11-25 13:19:26 +0800

    Update.

--- routes/api.js
+++ routes/api.js
@@ -17,6 +17,32 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
+// route middleware to verify a token
+app.use(function(req, res, next) {
+	var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+	// decode token
+	if (token) {
+
+		// verifies secret and checks exp
+		jwt.verify(token, app.get('superSecret'), function(err, decoded) {      
+			if (err) {
+		        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+			} else {
+		        // if everything is good, save to request for use in other routes
+		        req.decoded = decoded;    
+		        next();
+			}
+	    });
+	} else {
+    // if there is no token
+    // return an error
+		return res.status(403).send({ 
+	        success: false, 
+	        message: 'No token provided.' 
+		});
+	}
+});
 
 router.get('/adduser', function(req, res) {
 	var nick = new User({ 
